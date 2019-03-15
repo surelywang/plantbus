4f7803834c1488a305d4097eab610d6e76791248
Shirley Wang <surelywang@Shirleys-MacBook-Pro-2.local>
2019-03-15 00:38:19 -0700

    splash screen activity added

--- app/src/main/java/com/example/surelywang/plantbus/LogoPage.java
+++ app/src/main/java/com/example/surelywang/plantbus/LogoPage.java
@@ -1,16 +1,27 @@
 package com.example.surelywang.plantbus;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class LogoPage extends AppCompatActivity {
 
+    private static int SPLASH_TIME_OUT = 3000;  // 3 second delay
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Login page will start once timer is over
+                Intent intent = new Intent(LogoPage.this, LoginPage.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+
     }
 }
